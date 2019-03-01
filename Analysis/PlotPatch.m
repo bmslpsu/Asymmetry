de198a82ca4bb2289af7bc70a4b259325bc94d88
boc5244 <31375763+boc5244@users.noreply.github.com>
2019-02-28 22:02:00 -0500

    Update PlotPatch.m

--- Analysis/PlotPatch.m
+++ Analysis/PlotPatch.m
@@ -1,15 +1,14 @@
-%% FUNCTION:    PatchSTD
 function [h1,h2] = PlotPatch(dataMean,dataSTD,dataX,nSTD,nFly,lineColor,faceColor,alphaVal,lineWidth)
-%---------------------------------------------------------------------------------------------------------------------------------
-% PatchSTD: Plots patch for data mean
-    % INPUTS:
-        % dataMean: mean of data
-        % dataSTD: std of data
-      	% dataX: x-axis points
-        % edgeColor: patch edge color
-        % faceColor: pathc area color
-	% OUTPUTS
-        % h: curve & patch handles
+%% PlotPatch: Plots mean & STD as patch
+%   INPUTS:
+%       dataMean    : mean of data
+%       dataSTD     : std of data
+%       dataX       : x-axis points
+%       edgeColor   : patch edge color
+%       faceColor   : pathc area color
+%   OUTPUTS
+%       h1          : patch handle
+%       h2          : mean curve handle
 %---------------------------------------------------------------------------------------------------------------------------------      
     uE = dataMean + nSTD*dataSTD./sqrt(nFly);
     lE = dataMean - nSTD*dataSTD./sqrt(nFly);
@@ -18,5 +17,4 @@ function [h1,h2] = PlotPatch(dataMean,dataSTD,dataX,nSTD,nFly,lineColor,faceColo
     h1 = patch(xP,yP,1,'facecolor',faceColor,'edgecolor',lineColor);
     h2 = plot(dataX,dataMean,'Color',lineColor,'LineWidth',lineWidth);
     alpha(h1,alphaVal)
-%---------------------------------------------------------------------------------------------------------------------------------
 end
