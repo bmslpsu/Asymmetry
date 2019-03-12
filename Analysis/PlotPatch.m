0b995ea27b380abd1a44751ce0f7a82beda9375d
Benjamin Cellini <31375763+boc5244@users.noreply.github.com>
2019-03-12 19:43:20 -0400

    Update PlotPatch.m

--- Analysis/PlotPatch.m
+++ Analysis/PlotPatch.m
@@ -1,20 +1,25 @@
 function [h1,h2] = PlotPatch(dataMean,dataSTD,dataX,nSTD,nFly,lineColor,faceColor,alphaVal,lineWidth)
-%% PlotPatch: Plots mean & STD as patch
+%% PlotPatch: Plots mean/med & STD as patch
 %   INPUTS:
-%       dataMean    : mean of data
+%       dataMean    : mean/med data
 %       dataSTD     : std of data
 %       dataX       : x-axis points
+%       nSTD        : # of STD's to plot for patch
+%       nFly        : # of animals for standard error (nFly=1 for STD)
+%       lineColor  	: color of mean/med data
 %       edgeColor   : patch edge color
 %       faceColor   : pathc area color
+%       alphaVal    : patch transparancy (0-1)
+%       lineWidth  	: linewidth of main curve
 %   OUTPUTS
 %       h1          : patch handle
 %       h2          : mean curve handle
 %---------------------------------------------------------------------------------------------------------------------------------      
-    uE = dataMean + nSTD*dataSTD./sqrt(nFly);
-    lE = dataMean - nSTD*dataSTD./sqrt(nFly);
-    yP = [lE;flipud(uE)];
-    xP = [dataX;flipud(dataX)];
-    h1 = patch(xP,yP,1,'facecolor',faceColor,'edgecolor',lineColor);
-    h2 = plot(dataX,dataMean,'Color',lineColor,'LineWidth',lineWidth);
-    alpha(h1,alphaVal)
+uE = dataMean + nSTD*dataSTD./sqrt(nFly);
+lE = dataMean - nSTD*dataSTD./sqrt(nFly);
+yP = [lE;flipud(uE)];
+xP = [dataX;flipud(dataX)];
+h1 = patch(xP,yP,1,'facecolor',faceColor,'edgecolor',lineColor);
+h2 = plot(dataX,dataMean,'Color',lineColor,'LineWidth',lineWidth);
+alpha(h1,alphaVal)
 end
